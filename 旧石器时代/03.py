37abb5a16577c3a809869c29b19845399a11d844
buxingxing <305526954@qq.com>
2021-02-04 17:03:06 +0800

    增加说明

--- 旧石器时代/03.py
+++ 旧石器时代/03.py
@@ -1,5 +1,9 @@
 from jenkins_cli import jenkins_cli
 
+# 如果我们使用jenkins的方式是一个服务对应一个job
+# 那么如果服务很多就会有很多job，都在一个页面上不好查看
+# 这时候可以通过view来管理，通过view来把job分组
+# 这个view的划分需要根据实际情况，比如同一部门的job放在一起？或者同一种语言的job放在一起？等等
 
 # 创建一个view叫做dev
 # 传None就是使用默认配置
--- 旧石器时代/04.py
+++ 旧石器时代/04.py
@@ -4,6 +4,14 @@ from time import sleep
 
 job_name = 'test'
 
+# jenkins运行job是先把job放到队列中，然后再调度运行
+# 因此调用运行job的方法返回的是队列号
+# 之后需要拿队列号去获取运行会的构建号
+# 其它操作都需要这个构建号当参数
+# 在jenkins文档中说，这个队列号会在job运行完成后，保留5分钟
+# 所以需要抓紧获取
+# 我这里使用最蠢的while循环来获取
+# 真正的程序中应该修改这个逻辑，这里只是展示基本功能而已
 queue_number = jenkins_cli.run_job(job_name, {})
 
 while True:
